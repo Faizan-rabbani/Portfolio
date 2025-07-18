addfce6a94b1a83a3bbe862eda0a5d48c08343bc
Faizan Rabbani <faizanrabbani987@gmail.com>
2025-07-18 21:55:00 +0500

    newupdates

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
     document.getElementById('content').style.display = 'block';
     document.body.classList.add('loaded');
-  }, 1000); // Loader will stay for 1 second
+  }, 1000);
 });
